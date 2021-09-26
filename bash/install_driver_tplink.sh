0e178d820e22e992aff41896f580979008e6a08d
knight <jmen.org@gmail.com>
2021-09-26 22:16:11 +0300

    add new functionality

--- bash/install_driver_tplink.sh
+++ bash/install_driver_tplink.sh
@@ -1,8 +1,13 @@
-sudo dnf install kernel-devel
-mkdir ~/Downloads
+#sudo dnf install kernel-devel
+
+if [ -e ~/Downloads ]; then
+  echo "Directory exist!"
+else mkdir ~/Downloads
+fi
+
 cd  ~/Downloads
+rm -rf ~/Downloads/RTL88x2BU-Linux-Driver/
 git clone https://github.com/RinCat/RTL88x2BU-Linux-Driver.git
-git pull
 cd RTL88x2BU-Linux-Driver/
 make
 sudo make install
