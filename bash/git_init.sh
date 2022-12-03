f5f0e21e692b65e1b9fa6be9870581aff2e1e827
Michael Ch <jmen.org@gmail.com>
2022-12-03 19:21:17 +0300

    add new str to file

--- bash/git_init.sh
+++ bash/git_init.sh
@@ -4,3 +4,8 @@ git config --global user.email "jmen.org@gmail.com"
 git config --global user.name "Michael Ch"
 eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/id_rsa
+
+# generation key pair
+
+ssh-keygen -t ed25519 -C "your_email@example.com"
+ssh-keygen -t rsa -b 4096 -C "your_email@example.com"
