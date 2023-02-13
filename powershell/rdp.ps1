59dc11d1d01aa26e65838a84267112ec335884db
Michael Ch <jmen.org@gmail.com>
2023-02-13 20:49:49 +0300

    add script to rdp port

---
+++ powershell/rdp.ps1
@@ -0,0 +1,9 @@
+# https://learn.microsoft.com/ru-ru/windows-server/remote/remote-desktop-services/clients/change-listening-port
+# Show listner port
+Get-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber"
+
+#Change listner port
+$portvalue = 3390
+Set-ItemProperty -Path 'HKLM:\SYSTEM\CurrentControlSet\Control\Terminal Server\WinStations\RDP-Tcp' -name "PortNumber" -Value $portvalue 
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-TCP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol TCP -LocalPort $portvalue 
+New-NetFirewallRule -DisplayName 'RDPPORTLatest-UDP-In' -Profile 'Public' -Direction Inbound -Action Allow -Protocol UDP -LocalPort $portvalue
