d1a355b522a1bab550fb4a4f43fed52a2fc1e705
Michael Ch <jmen.org@gmail.com>
2023-05-08 20:15:34 +0300

    change script

--- bash/virtualbox_inst.sh
+++ bash/virtualbox_inst.sh
@@ -1,12 +1,28 @@
 #!/usr/bin/env bash
 
 #https://www.virtualbox.org/wiki/Linux_Downloads
-release_name=$(lsb_release -a | grep -i codename|cut -f2-)
-echo $release_name
-wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
-echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ' $release_name ' contrib' > ~/tmp/virtualbox.list
-mkdir -p ~/tmp
-cat ~/tmp/virtualbox.list
-sudo cp ~/tmp/vb.list /etc/apt/sources.list.d/virtualbox.list
-sudo cat /etc/apt/sources.list.d/virtualbox.list
-sudo apt update
+
+release=$(lsb_release -a | grep Distributor|cut -f2-)
+echo $release
+#Debian/Ubuntu
+#add key
+if [ "$release" = Ubuntu ] || [ "$release" = Debian ]
+then
+  echo "Your release is" $release
+  release_name=$(lsb_release -a | grep -i codename|cut -f2-)
+  echo "Your release name is" $release_name
+  wget -O- https://www.virtualbox.org/download/oracle_vbox_2016.asc | sudo gpg --dearmor --yes --output /usr/share/keyrings/oracle-virtualbox-2016.gpg
+  echo 'deb [arch=amd64 signed-by=/usr/share/keyrings/oracle-virtualbox-2016.gpg] https://download.virtualbox.org/virtualbox/debian ' $release_name ' contrib' > /tmp/virtualbox.list
+  cat /tmp/virtualbox.list
+  sudo mv ~/tmp/virtualbox.list /etc/apt/sources.list.d/virtualbox.list
+  sudo cat /etc/apt/sources.list.d/virtualbox.list
+  sudo apt update
+  #install prerequimentes
+elif [ "$release" = Centos ] || [ "$release" = Fedora ]
+then
+  dnf check-update
+  #install prerequimentes
+  echo "Please install in non-automatic process"
+
+fi
+
